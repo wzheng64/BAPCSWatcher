2228ed65edff37e55a8d4208b9032ce46693cc38
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 16:14:39 -0400

    Added commands and main file

---
+++ commands/watch.js
@@ -0,0 +1,26 @@
+module.exports = {
+	name: 'watch',
+	description: 'Set a watch for a deal',
+	args: true,
+	usage: '<Product Type>',
+	cooldown: 10,
+	execute(message, args) {
+		const commandName = args[0].toLowerCase();
+		const command = message.client.commands.get(commandName) ||
+			message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+		if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+
+		delete require.cache[require.resolve(`./${command.name}.js`)];
+
+		try {
+			const newCommand = require(`./${command.name}.js`);
+			message.client.commands.set(newCommand.name, newCommand);
+			message.channel.send(`Command \`${command.name}\` was reloaded!`);
+		}
+		catch (error) {
+			console.log(error);
+			message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+		}
+	},
+};
---
+++ index.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const snoowrap = require('snoowrap');
+const { prefix, token, clientId, clientSecret, redditUsername, redditPW } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+const r = new snoowrap({
+	userAgent: 'test /u/username',
+	clientId: clientId,
+	clientSecret: clientSecret,
+	username: redditUsername,
+	
+});
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	// Checks to make sure the command sent has the proper prefix and is from a USER
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	// First word is considered the command and the rest of the args contain the arguments
+	const commandName = args.shift().toLowerCase();
+
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('I can\'texecute that command inside DMs!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	// A variable that gets the necessary cooldown amount. If you don't supply it in the command file
+	// it will default to 3. The 1000 converts ms to seconds.
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	// Try to execute the command
+	try {
+		command.execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+setTimeout(())
+
+client.login(token);
