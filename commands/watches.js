29e37e48fee607a9f376a000966f146d313d898b
BuildTools <unconfigured@null.spigotmc.org>
2020-07-22 15:18:21 -0400

    Initial watches command

--- commands/watches.js
+++ commands/watches.js
@@ -1,23 +1,35 @@
+const fs = require('fs');
+
 module.exports = {
-	name: 'watch',
-	description: 'Set a watch for a deal',
+	name: 'watches',
+	description: 'Reports back all your current watches',
+	// eslint-disable-next-line no-unused-vars
 	execute(message, args) {
-		const commandName = args[0].toLowerCase();
-		const command = message.client.commands.get(commandName)
-			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-		if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
-
-		delete require.cache[require.resolve(`./${command.name}.js`)];
-
-		try {
-			const newCommand = require(`./${command.name}.js`);
-			message.client.commands.set(newCommand.name, newCommand);
-			message.channel.send(`Command \`${command.name}\` was reloaded!`);
-		}
-		catch(error) {
-			console.log(error);
-			message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+		let watches = [];
+		if (!args.length) {
+			const id = message.author.id;
+			watches.push('Here\'s a list of all your current watches.');
+			fs.readFile('./data/watch.json', 'utf8', (err, data) => {
+				if (err) throw err;
+				watches = JSON.parse(data).data;
+				watches.forEach(e => {
+					if (e.madeBy.id == id) {
+						watches.push(`Product type: ${e.type}	Price: ${e.price}	Other specs: ${e.other.join(', ')}	Expires on: ${e.expiresOn}`);
+					}
+				});
+				// Attempt to DM the requesting user command information
+				// the split parameter will try to break up the info
+				// if it exceeds 2000 characters
+				return message.author.send(data, { split: true })
+					.then(() => {
+						if (message.channel.type === 'dm') return;
+						message.reply('I\'ve send you a DM with all of your watches!');
+					})
+					.catch(error => {
+						console.error(`Could not send DM to ${message.author.tag}.\n`, error);
+						message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+					});
+			});
 		}
 	},
 };
