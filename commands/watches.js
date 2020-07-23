420480f646fa06d47d48dcc0cac21512e465bab7
BuildTools <unconfigured@null.spigotmc.org>
2020-07-23 19:01:49 -0400

    fixed watches command and message format

--- commands/watches.js
+++ commands/watches.js
@@ -5,22 +5,27 @@ module.exports = {
 	description: 'Reports back all your current watches',
 	// eslint-disable-next-line no-unused-vars
 	execute(message, args) {
-		let watches = [];
+		const months = ['January', 'February', 'March', 'April', 'May', 'June',
+			'July', 'August', 'September', 'October', 'November', 'December'
+		];
+		const alerts = [];
 		if (!args.length) {
 			const id = message.author.id;
-			watches.push('Here\'s a list of all your current watches.');
-			fs.readFile('./data/watch.json', 'utf8', (err, data) => {
+			alerts.push('Here\'s a list of all your current watches.');
+			fs.readFile('./data/watches.json', 'utf8', (err, data) => {
 				if (err) throw err;
-				watches = JSON.parse(data).data;
+				const watches = JSON.parse(data).data;
 				watches.forEach(e => {
-					if (e.madeBy.id == id) {
-						watches.push(`Product type: ${e.type}	Price: ${e.price}	Other specs: ${e.other.join(', ')}	Expires on: ${e.expiresOn}`);
+					if (e.madeBy == id) {
+						const date = new Date(e.expiresOn);
+						const dateString = `${months[date.getUTCMonth()]} ${date.getUTCDate()}, ${date.getUTCFullYear()}`;
+						alerts.push(`Product type: ${e.type}	Price: $${e.price}	Other specs: ${e.other.join(', ')}	Expires on: ${dateString}`);
 					}
 				});
 				// Attempt to DM the requesting user command information
 				// the split parameter will try to break up the info
 				// if it exceeds 2000 characters
-				return message.author.send(data, { split: true })
+				return message.author.send(alerts.join('\n'), { split: true })
 					.then(() => {
 						if (message.channel.type === 'dm') return;
 						message.reply('I\'ve send you a DM with all of your watches!');
